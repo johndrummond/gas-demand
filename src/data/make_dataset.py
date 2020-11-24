c63aa9616b9b11268ef787dc50aeeffd60f2c64a
John Drummond <johnhhd@gmail.com>
2020-11-24 21:23:28 +0000

    post initial exploration

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import pandas as pd
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
@@ -11,9 +12,31 @@ from dotenv import find_dotenv, load_dotenv
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
+        Loads raw CSV, imputes missing values and stores in processed
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    # load file
+    gas_src = pd.read_csv(input_filepath)
+    
+    # rename columns
+    gas_src.columns = ['day','s1_deg_c','s2_deg_c','s1_wind','s2_wind','gas_demand']
+    
+    # parse and set date index
+    gas_src['day'] = pd.to_datetime(gas_src['day'],format="%d/%m/%Y")
+    gas_src = gas_src.set_index('day')
+
+    # fill in missing dates
+    alldays = pd.date_range(
+        start = gas_src.first_valid_index(), 
+        end = gas_src.last_valid_index() )
+    gas_src = gas_src.reindex(alldays)
+
+    # interpolate missing data
+    gas_src = gas_src.apply(lambda x: x.interpolate(method='time'), axis=0)
+
+    # save processed dataset read for forecasting
+    gas_src.to_feather(output_filepath)
 
 
 if __name__ == '__main__':
